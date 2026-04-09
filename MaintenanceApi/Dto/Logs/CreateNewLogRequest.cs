8bda7e7ccf6d96c1384f31bd8023f9e6318adad8
srankoin <srankoin@gmail.com>
2026-04-09 18:05:14 -0500

    logs

---
+++ MaintenanceApi/Dto/Logs/CreateNewLogRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace MaintenanceApi.Dto.Logs
+{
+    public class CreateNewLogRequest
+    {
+        public string event_type { get; set; }
+        public string event_action { get; set; }
+        public string entity_id { get; set; }
+        public string description { get; set; }
+        public string performed_by { get; set; }
+    }
+}
---
+++ MaintenanceApi/Dto/Logs/LogResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace MaintenanceApi.Dto.Logs
+{
+    public class LogResponse
+    {
+        public int event_id { get; set; }
+        public string event_type { get; set; }
+        public string event_action { get; set; }
+        public string entity_id { get; set; }
+        public string description { get; set; }
+        public string performed_by { get; set; }
+        public DateTime created_at { get; set; }
+    }
+}
